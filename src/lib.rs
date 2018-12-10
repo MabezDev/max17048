47394d206cbec808e054bd8be85db01dd3a6e745
Scott <MabezDev@gmail.com>
2018-12-10 00:03:34 +0000

    Added option to set rcomp - compensation

--- src/lib.rs
+++ src/lib.rs
@@ -1,9 +1,10 @@
 
 #![no_std]
 
-use embedded_hal::blocking::i2c::WriteRead;
+use embedded_hal::blocking::i2c::{WriteRead, Write};
 
 const MAX17048_ADDR: u8 = 0x6C;
+const DEFAULT_RCOMP: u8 = 0x97;
 
 
 pub struct Max17048<I> {
@@ -13,15 +14,17 @@ pub struct Max17048<I> {
 
 
 impl<I, E> Max17048<I> 
-where I: WriteRead<Error = E>,
+where I: WriteRead<Error = E> + Write<Error = E>,
       E: core::fmt::Debug
 {
 
     pub fn new(i2c: I) -> Self {
-        Max17048 {
+        let mut max = Max17048 {
             i2c: i2c,
             recv_buffer: [0u8; 2]
-        }
+        };
+        max.compensation(DEFAULT_RCOMP).unwrap();
+        max
     }
 
     pub fn version(&mut self) -> Result<u16, E> {
@@ -38,7 +41,9 @@ where I: WriteRead<Error = E>,
     /// Return C/Rate in %/hr
     pub fn charge_rate(&mut self) -> Result<f32, E> {
         match self.read(0x16) {
-            Ok(val) => Ok(val as f32 * 0.208),
+            Ok(val) => { 
+                Ok(val as f32 * 0.208)
+            },
             Err(e) => Err(e)
         }
     }
@@ -46,6 +51,24 @@ where I: WriteRead<Error = E>,
     pub fn vcell(&mut self) -> Result<f32, E> {
         match self.read(0x02) {
             Ok(val) => Ok(val as f32 * 0.000078125),
+            Err(e) => Err(e)
+        }
+    }
+
+    fn compensation(&mut self, rcomp: u8) -> Result<(), E>{
+        // read the current reg vals
+        match self.read(0x02) {
+            Ok(mut value) => {
+                value &= 0x00FF;
+                value |= (rcomp as u16) << 8;
+                // write to the rcomp bits only
+                self.i2c.write(MAX17048_ADDR, &[0x0C])?;
+                let msb = ((value & 0xFF00) >> 8) as u8;
+                let lsb = ((value & 0x00FF) >> 0) as u8;
+                self.i2c.write(MAX17048_ADDR, &[msb])?;
+                self.i2c.write(MAX17048_ADDR, &[lsb])?;
+                Ok(())
+            },
             Err(e) => Err(e)
         }
     }
